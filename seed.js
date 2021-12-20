629f53615a622b63375e68c3bce8d4e807501010
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-12-20 14:41:44 -0600

    sets up database and seed and clear js files, db is working properly from back end

---
+++ seed.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Event = require('./modules/eventModel.js');
+
+async function seed() {
+  mongoose.connect(process.env.DB_URL);
+  await Event.create({
+    name: 'coffee with friends',
+    startTime: '12/20/2021 1400', // TODO: possibly not a String, required
+    endTime: '12/20/2022 1500', // not required
+    address: '1234 This st', // TODO: is this better as location? URLs for virtual events?
+    description: 'drinking coffee', // not required 
+    link: 'https://www.google.com/search?q=coffee+with+friends&rlz=1C1VDKB_enUS935US935&sxsrf=AOaemvLpuxDRcIF6tGktgAIsA8nrFunn2g:1640032414304&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj-3-HrnPP0AhWKmWoFHTS0BEgQ_AUoAXoECBAQAw&biw=1858&bih=977&dpr=1#imgrc=yBDjU-BgawGhIM',
+    image: 'thisidontcare.com'
+  });
+  console.log('coffee with friends');
+  // new book
+  await Event.create({
+    name: 'coffee with enemies',
+    startTime: '12/20/2023 1400', // TODO: possibly not a String, required
+    endTime: '12/20/2022 1500', // not required
+    address: '1234 This st', // TODO: is this better as location? URLs for virtual events?
+    description: 'drinking coffee, and hating one another', // not required 
+    link: 'https://www.google.com/search?q=coffee+with+friends&rlz=1C1VDKB_enUS935US935&sxsrf=AOaemvLpuxDRcIF6tGktgAIsA8nrFunn2g:1640032414304&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj-3-HrnPP0AhWKmWoFHTS0BEgQ_AUoAXoECBAQAw&biw=1858&bih=977&dpr=1#imgrc=yBDjU-BgawGhIM',
+    image: 'thisidontcare.com'
+  });
+  console.log('coffee with enemies');
+  mongoose.disconnect();
+}
+seed();
+
+// make sure server is NOT running
+// in terminal go to project root
+// type 'node seed.js'
---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+// app.get(); // retrieves data
+// app.post(); // creates data
+// app.delete(); // deletes data
+// app.put(); // updates data
+
+
+
+
+
+app.listen(PORT, () => console.log('listening on port: ' + PORT))
