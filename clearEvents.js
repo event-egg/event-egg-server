319e2f0eb57faeb9a087bafceb79226455d48298
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-12-21 10:46:40 -0600

    properly posting events data and saving it within User instance on DB

--- clearEvents.js
+++ clearEvents.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const Event = require('./models/eventModel');
+const Event = require('./models/eventModel.js');
+const User = require('./models/userModel.js');
 
 async function clear() {
   mongoose.connect(process.env.DB_URL);
   try {
     await Event.deleteMany({});
+    await User.deleteMany({});
     console.log('events cleared');
   } catch (err) {
     console.error(err);
--- modules/postEvent.js
+++ modules/postEvent.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Event = require('../models/eventModel.js');
+// const Event = require('../models/eventModel.js');
 const User = require('../models/userModel.js');
 
 async function postEvent(request, response) {
@@ -9,9 +9,14 @@ async function postEvent(request, response) {
   //     response.send('invalid token');
   //   } else {
   try {
-    const madeEvent = await Event.create({ ...request.body }); // email: user.email, add this later after verification
-    await User.findByIdAndUpdate("61c1ecd9f2b3369e5fe618b8", { savedEvents: madeEvent })
-    response.status(200).send(madeEvent);
+    // const madeEvent = await Event.create({ ...request.body }); // email: user.email, add this later after verification
+    const worked = await User.findByIdAndUpdate("61c203d438ce64efd20c215b", {
+      $push: {
+        savedEvents: { ...request.body }
+      }
+    })
+    console.log(worked);
+    response.status(200).send(worked);
   } catch (err) {
     response.status(500).send('server error');
   }
