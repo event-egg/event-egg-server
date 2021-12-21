b26c33c6f7b996462a2c97875d2a0108b9959857
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-12-21 15:38:15 -0600

    adds update user patch() method

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const User = require('./models/userModel.js');
 const getUserData = require('./modules/getUserData.js');
 const createEvent = require('./modules/createEvent.js');
 const createUser = require('./modules/createUser.js');
@@ -29,6 +30,22 @@ app.get('/user', getUserData); // retrieves data for just one user from DB
 app.post('/user', createUser); // creates user on DB
 app.post('/events/:id', createEvent); // creates data, when a user 'likes' an event, it will create an Event saved to their profile here in the DB
 // app.delete(); // deletes data
-// app.put('/user/:id', updateUserPreferences); // updates data
+app.patch('/user/:id', updateUserPreferences); // updates data
+
+async function updateUserPreferences(request, response) {
+  // verifyUser(request, (err, user) => {
+  //   if (err) {
+  //     response.send('invalid token');
+  //   } else {
+  const { id } = request.params;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, request.body, { new: true, overwrite: false });
+    response.status(200).send(updatedUser);
+  } catch (err) {
+    response.status(500).send('server error');
+  }
+  //   }
+  // })
+}
 
 app.listen(PORT, () => console.log('listening on port: ' + PORT))
