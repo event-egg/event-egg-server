594c89bb0d2d830657913596aeb45dd2f4dcd792
Kellen-Linse <kblinse06@gmail.com>
2021-12-22 17:30:03 -0800

    adds auth to getUser route

--- modules/getUserData.js
+++ modules/getUserData.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const User = require('../models/userModel.js');
+const verifyUser = require('../auth');
 
 async function getUserData(request, response) {
   console.log('get hit')
-  // verifyUser(request, async (err, user) => {
-  //   if (err) {
-  //     response.send('invalid token');
-  //   } else {
-  const { email } = request.query;
-  try {
-    let retrievedUser = await User.findOne({ email: email });
-    retrievedUser === null ? response.status(200).send({}) : response.status(200).send(retrievedUser);
-  } catch (err) {
-    console.log(err);
-    response.status(500).send('server error');
-  }
-
-  //   }
-  // })
+  verifyUser(request, async (err, user) => {
+        if (err) {
+          response.send('invalid token');
+        } else {
+      const { email } = request.query;
+      try {
+        let retrievedUser = await User.findOne({ email: email });
+        retrievedUser === null ? response.status(200).send({}) : response.status(200).send(retrievedUser);
+      } catch (err) {
+        console.log(err);
+        response.status(500).send('server error');
+      }
+    }
+  })
 }
 
 module.exports = getUserData;
