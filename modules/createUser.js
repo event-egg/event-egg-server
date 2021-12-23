326ec91179bfb61eecb524ef455d4ddc63c69bd0
Kellen-Linse <kblinse06@gmail.com>
2021-12-22 18:03:05 -0800

    Add auth to createUser

--- modules/createUser.js
+++ modules/createUser.js
@@ -1,21 +1,22 @@
 'use strict';
 
 const User = require('../models/userModel.js');
+const verifyUser = require('../auth');
 
 async function createUser(request, response) {
-  // verifyUser(request, (err, user) => {
-  //   if (err) {
-  //     response.status(500).send(err);
-  //     response.send('invalid token');
-  //   } else {
+  verifyUser(request, async (err, user) => {
+    if (err) {
+      response.status(500).send(err);
+      response.send('invalid token');
+    } else {
   try {
     const madeUser = await User.create({ ...request.body });
     response.status(200).send(madeUser);
   } catch (err) {
     response.status(500).send('server error');
   }
-  //   }
-  // })
+    }
+  })
 }
 
 module.exports = createUser;
